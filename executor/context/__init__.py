1653502c4e8e6658d1ba63d2a70b5e7cfc179021
Ivan Usenka <vanek66650@gmail.com>
2018-09-03 18:43:55 +0300

    refactoring for context initialization on Executor's init

--- executor/context/__init__.py
+++ executor/context/__init__.py
@@ -1,1 +1,2 @@
 from spark_hive_context import SparkHiveContext
+spark_context = SparkHiveContext('app', 'thrift://sandbox-hdp.hortonworks.com:9083')
--- executor/spark_hive_executor.py
+++ executor/spark_hive_executor.py
@@ -1,16 +1,11 @@
-from executor.context import SparkHiveContext
+from executor.context import spark_context as sc
 
 
 class SparkHiveExecutor:
-
-    __app_name = 'app'
-    __hive_metastore_adr = 'thrift://sandbox-hdp.hortonworks.com:9083'
-    __sc = SparkHiveContext(__app_name, __hive_metastore_adr)
 
     def __init__(self):
         pass
 
     @staticmethod
     def execute_sql(sql_string):
-        df = SparkHiveExecutor.__sc.get_session().sql(sql_string)
-        return df
+        return sc.get_session().sql(sql_string)
